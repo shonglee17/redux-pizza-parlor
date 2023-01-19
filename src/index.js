87c1552556d6bf517f1684d8986f97058861aaf2
Cole Dahlen <coledahlen@gmail.com>
2023-01-19 14:13:00 -0600

    added redux to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    combineReducers({
+        
+    }),
+    applyMiddleware(
+        logger
+    )
+ )
+ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+ );
+ 
