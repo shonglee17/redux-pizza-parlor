0ce11a7ece3270b084f9a5bbe888d282860a2cc2
Cole Dahlen <coledahlen@gmail.com>
2023-01-19 15:07:36 -0600

    completed the get route and put it in redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import SelectPizza from './SelectPizza';
 import CustomerInformation from './CustomerInformation';
@@ -8,6 +9,23 @@ import Checkout from './Checkout';
 import Admin from './Admin'
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {fetchPizza()},[]);
+  const fetchPizza = () => {
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    }).then ((response) => {
+      console.log(response.data)
+      dispatch({
+        type: 'GET_PIZZA',
+        payload: response.data
+    })
+
+    }).catch((error) => {
+      console.log('useEffect failed:', error);
+    })
+}
 
 
     return (
